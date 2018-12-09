73e12b6d0cefe8ac1f54c1f5794f57a22e336594
Kazucoin <45504088+kazucoin@users.noreply.github.com>
2018-12-10 07:58:42 +0800

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -264,11 +264,21 @@ unsigned int GetNextWorkRequiredV4fix(const CBlockIndex* pindexLast, const Conse
 	int64_t nActualTimespan = pindexLast-> GetMedianTimePast() - pindexFirst->GetMedianTimePast();
 	nActualTimespan = params.nAveragingTargetTimespanV4fix + (nActualTimespan - params.nAveragingTargetTimespanV4fix)/4;
 
-	if (nActualTimespan < params.nMinActualTimespanV4fix)
-		nActualTimespan = params.nMinActualTimespanV4fix;
-	if (nActualTimespan > params.nMaxActualTimespanV4fix)
-		nActualTimespan = params.nMaxActualTimespanV4fix;
-
+	if (pindexLast->nHeight < 117014)
+	{
+		if (nActualTimespan < params.nMinActualTimespanV4)
+ 			nActualTimespan = params.nMinActualTimespanV4;
+ 		if (nActualTimespan > params.nMaxActualTimespanV4)
+ 			nActualTimespan = params.nMaxActualTimespanV4;
+	}
+	else
+	{
+		if (nActualTimespan < params.nMinActualTimespanV4fix)
+			nActualTimespan = params.nMinActualTimespanV4fix;
+		if (nActualTimespan > params.nMaxActualTimespanV4fix)
+			nActualTimespan = params.nMaxActualTimespanV4fix;
+	}
+	
 	//Global retarget
 	arith_uint256 bnNew;
 	bnNew.SetCompact(pindexPrevAlgo->nBits);
